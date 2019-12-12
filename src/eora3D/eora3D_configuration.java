82718f5bc584a9d54e8f8ed74663201d906df409
Nick Hoath <nick.hoath@gmail.com>
2019-12-12 00:10:31 +0000

    Added skeleton for storable configuration class

---
+++ src/eora3D/eora3D_configuration.java
@@ -0,0 +1,18 @@
+package eora3D;
+
+import java.io.File;
+import java.io.Serializable;
+
+class eora3D_configuration_version implements Serializable
+{
+	public int sm_config_version = 1;
+}
+
+class eora3D_configuration_data_v1 implements Serializable
+{
+	public int sm_laser_0_offset = 1800;
+	public int sm_laser_steps_per_deg = 80;
+	
+	public File sm_config_file = new File("ScannerConfig.e3d");
+}
+
