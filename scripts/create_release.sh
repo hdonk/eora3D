d5cbfa4ff88b0c9084963df8a9fd31cd10860a95
Nick Hoath <nick.hoath@gmail.com>
2020-02-14 13:11:00 +0000

    Cleaned up some UI code Added in release generating script

--- scripts/create_release.sh
+++ scripts/create_release.sh
@@ -1,2 +1,6 @@
-#!/bin/sh
-tar cvJf e3d_release.tar.xz `find eora3D jPLY/ pointCloudViewer/ -iname \*glsl -or -iname \*txt -or -iname LICENSE -or -iname \*class -or -iname \*\.so\* -or -iname \*\.dll`
+#!/bin/bash
+set -e
+cp eora3D/scripts/*sh .
+FILES=`find eora3D jPLY/ pointCloudViewer/ -iname \*glsl -or -iname \*txt -or -iname LICENSE -or -iname \*class -or -iname \*\.so\* -or -iname \*\.dll -or -iname \*jar`
+FILES=`echo "$FILES" | grep -v -e javadoc -e source`
+tar cvJf e3d_release.tar.xz ${FILES} run_*.sh
