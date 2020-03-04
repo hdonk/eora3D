2991fa83868ef3ac80041e61b9c029d348676082
Nick Hoath <nick.hoath@gmail.com>
2020-03-04 14:11:28 +0000

    Start supporting Windows 10 BLE through javelin

---
+++ src/eora3D/eora3D_bluetooth_javelin.java
@@ -0,0 +1,12 @@
+package eora3D;
+import javelin.javelin;
+
+public class eora3D_bluetooth_javelin extends eora3D_bluetooth {
+	javelin m_javelin;
+ 
+	public eora3D_bluetooth_javelin()
+	{
+		m_javelin = new javelin();
+	}
+
+}
